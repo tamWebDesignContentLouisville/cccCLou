8dce414c49d5f912bc7f16753135221e90b5e31a
Tammy Shaw <tammy@protempus.com>
2016-03-15 19:49:46 -0400

    changes after parallax. 8pm 3/15/16.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
-$(".slideShow img").click(function(event){
-    var smlImg = $(this).attr("src");
-    var  lrgImg = $(".lrgImg").attr("src");
-      $(".lrgImg").attr("src",smlImg);
-});
-});
 
+function parallax(){
+	var prlx_lyr_0 = document.getElementById('prlx_lyr_0');	
+	var prlx_lyr_1 = document.getElementById('prlx_lyr_1');
+	var prlx_lyr_2 = document.getElementById('prlx_lyr_2');
+	prlx_lyr_0.style.top = -(window.pageYOffset / 4)+'px';	
+	prlx_lyr_1.style.top = 710 -(window.pageYOffset / 4)+'px';
+	prlx_lyr_2.style.top = 1420 -(window.pageYOffset / 4)+'px';
+}
+window.addEventListener("scroll", parallax, false);
 
+$( document ).ready(function() {
+	$("#image").empty();		
+	$.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+		{
+			  tags: "catholic students",
+			  tagmode: "any",
+			  format: "json"
+		}, function(data){
+			$.each(data.items, function(i, item){
+			
+				$('<img/>').attr("src", item.media.m).appendTo('#images');
+				if(i==4) return false;
+		});	  
+	});
+	parallax();
+	
+});
